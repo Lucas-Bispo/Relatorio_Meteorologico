3cf8bc648b1d133bef297ff7439510151e01b4ce
Lucas-Bispo <lukaobispo@gmail.com>
2020-07-05 09:48:17 -0300

    http,chave e edição na mai dar

--- web/main.dart
+++ web/main.dart
@@ -1,5 +1,25 @@
 import 'dart:html';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 void main() {
   
+}
+
+Future getWeather(String city){
+  String url = 'https://api.hgbrasil.com/weather/?format=json-cors&locale=pt&city_name=$city&key=0170eb3e';
+  return http.get(url);
+}
+
+void loadData(List cities){
+  var empty = querySelector('#empty');
+
+  if(empty != null){
+    empty.remove();
+
+    cities.forEach((city){
+      insertData(getWeather(city));
+
+    });
+  }
 }
